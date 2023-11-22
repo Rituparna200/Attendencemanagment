9126f9b82171f4003fe647d744bf078de9c0d5cf
Rituparna200 <samanta002ritu@gmail.com>
2023-11-22 09:01:44 +0530

    Spring1 project

---
+++ src/test/java/com/Spring/AttendenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
